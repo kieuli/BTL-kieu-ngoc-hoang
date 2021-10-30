356f48ac83e7f6418b55c08a223841914b0ef4e7
your name <likieu2607@gmail.com>
2021-10-30 23:29:36 +0700

    bổ xung csdl liên kết các bảng

--- database/btl.sql
+++ database/btl.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 29, 2021 at 04:21 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 30, 2021 lúc 06:27 PM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,38 +18,40 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `btl`
+-- Cơ sở dữ liệu: `btl`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `menu_catalog`
+-- Cấu trúc bảng cho bảng `menu_catalog`
 --
 
 CREATE TABLE `menu_catalog` (
-  `id` int(11) NOT NULL,
+  `id_ca` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `metaTitle` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `menu_catalog`
+-- Đang đổ dữ liệu cho bảng `menu_catalog`
 --
 
-INSERT INTO `menu_catalog` (`id`, `name`, `metaTitle`, `created`, `updated`) VALUES
-(1, 'ngoc', 'ngoc', '2021-10-29 01:58:50', '2021-10-29 01:58:50');
+INSERT INTO `menu_catalog` (`id_ca`, `name`, `metaTitle`, `created`, `updated`, `id`) VALUES
+(3, 'meo meo', 'meo-meo', '2021-10-29 15:48:42', '2021-10-30 16:24:17', 3),
+(4, 'đời sống', 'doi-song', '2021-10-29 15:53:03', '2021-10-30 16:24:25', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `menu_list_blog`
+-- Cấu trúc bảng cho bảng `menu_list_blog`
 --
 
 CREATE TABLE `menu_list_blog` (
-  `id` int(11) NOT NULL,
+  `id_blog` int(11) NOT NULL,
   `images` varchar(300) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `metaTitle` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -57,27 +59,25 @@ CREATE TABLE `menu_list_blog` (
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `dateTime` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+  `dateTime` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `menu_list_blog`
+-- Đang đổ dữ liệu cho bảng `menu_list_blog`
 --
 
-INSERT INTO `menu_list_blog` (`id`, `images`, `name`, `metaTitle`, `catalog`, `description`, `content`, `token`, `dateTime`) VALUES
-(4, 'public/images/1635475247z2749690876366_a1f45a1b60c959504f0391d0463ed7eb.jpg', 'chân váy', 'chan-vay', '1', 'Nguyễn Yến Ngọc', '<p>Ss&Aacute;</p>\r\n', 'bxwWlykYsU', '29/10/2021'),
-(5, 'public/images/1635475508z2749690876366_a1f45a1b60c959504f0391d0463ed7eb.jpg', 'dấdad', 'dadad', '1', 'adádsa', '<p>d&acirc;d</p>\r\n', 'W30iQX1wyo', '29/10/2021'),
-(6, 'public/images/1635476357z2749690876366_a1f45a1b60c959504f0391d0463ed7eb.jpg', 'tesst', 'tesst', '1', 'd', '<p>dasd</p>\r\n', 'BcGmTzJPRI', '29/10/2021'),
-(7, 'public/images/16355170376659BAAC-51A8-4D54-9A57-B1034E073298.jpg', '61HT tung tung ta', '61HT-tung-tung-ta', '1', 'Nếu không', '<p>Thi&nbsp;</p>\r\n', 'x3QW5P1iUz', '29/10/2021');
+INSERT INTO `menu_list_blog` (`id_blog`, `images`, `name`, `metaTitle`, `catalog`, `description`, `content`, `token`, `dateTime`, `id`) VALUES
+(0, '', 'kakak', '', '', '', 'sdfghjgn', 'csfcs', '', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `users`
 --
 
 CREATE TABLE `users` (
-  `id_users` int(10) UNSIGNED NOT NULL,
+  `id` int(10) NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -86,62 +86,74 @@ CREATE TABLE `users` (
   `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `permission` int(1) NOT NULL,
   `activated` tinyint(1) DEFAULT 0,
-  `varkey` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+  `varkey` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `templates` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Đang đổ dữ liệu cho bảng `users`
 --
 
-INSERT INTO `users` (`id_users`, `name`, `email`, `password`, `token`, `created`, `updated`, `permission`, `activated`, `varkey`) VALUES
-(3, 'lâm thị kiều', 'likieu2607@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '', '2021-10-24 10:36:03', '2021-10-25 12:58:20', 0, 1, ''),
-(4, 'li kiều', '1951060022@e.tlu.edu.vn', 'fcea920f7412b5da7be0cf42b8c93759', '', '2021-10-24 10:36:51', '2021-10-25 12:58:40', 0, 1, ''),
-(5, 'yến ngọc', 'nyn11092001@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '', '2021-10-24 14:18:07', '2021-10-24 14:18:07', 0, 0, ''),
-(6, 'nguyenyenngoc', 'nguyenyenngoc61ht@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'wfFmPrmxFi', '2021-10-26 08:08:30', '2021-10-26 08:08:30', 0, 0, '');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `token`, `created`, `updated`, `permission`, `activated`, `varkey`, `templates`) VALUES
+(3, 'lâm thị kiều', 'likieu2607@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '', '2021-10-24 03:36:03', '2021-10-25 05:58:20', 0, 1, '', 0),
+(4, 'li kiều', '1951060022@e.tlu.edu.vn', 'fcea920f7412b5da7be0cf42b8c93759', '', '2021-10-24 03:36:51', '2021-10-25 05:58:40', 0, 1, '', 0),
+(5, 'yến ngọc', 'nyn11092001@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '', '2021-10-24 07:18:07', '2021-10-24 07:18:07', 0, 0, '', 0),
+(6, 'nguyenyenngoc', 'nguyenyenngoc61ht@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'wfFmPrmxFi', '2021-10-26 01:08:30', '2021-10-26 01:08:30', 0, 0, '', 0);
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `menu_catalog`
+-- Chỉ mục cho bảng `menu_catalog`
 --
 ALTER TABLE `menu_catalog`
+  ADD PRIMARY KEY (`id_ca`),
+  ADD KEY `id` (`id`);
+
+--
+-- Chỉ mục cho bảng `menu_list_blog`
+--
+ALTER TABLE `menu_list_blog`
+  ADD KEY `id` (`id`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `menu_list_blog`
---
-ALTER TABLE `menu_list_blog`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_users`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `menu_catalog`
+-- AUTO_INCREMENT cho bảng `menu_catalog`
 --
 ALTER TABLE `menu_catalog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_ca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `menu_list_blog`
---
-ALTER TABLE `menu_list_blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_users` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `menu_catalog`
+--
+ALTER TABLE `menu_catalog`
+  ADD CONSTRAINT `menu_catalog_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+
+--
+-- Các ràng buộc cho bảng `menu_list_blog`
+--
+ALTER TABLE `menu_list_blog`
+  ADD CONSTRAINT `menu_list_blog_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
