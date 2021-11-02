0c90afc9358121004f4da896fdb8b89bea748f75
your name <likieu2607@gmail.com>
2021-11-03 00:03:14 +0700

    hoàn thiện các trang nhán index

--- src/index.php
+++ src/index.php
@@ -15,31 +15,31 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
   <title>Blog cá nhân</title>
-  <link rel="stylesheet" href="../src/style.css">
+  <link rel="stylesheet" href="public/style.css">
 </head>
 <body>
   <div class="app">
     <header>
       <div class="nav">
         <a class="nav__logo" href="">
-          <img src="img/HKN.jpg" alt="">
+          <img src="img/HKN.jpg" alt="index.php">
         </a>
         <ul class="nav__menu">
           <li class="nav__menu-items">
-            <a class="nav__menu-items-link" href="">Trang chủ</a>
+            <a class="nav__menu-items-link" href="index.php">Trang chủ</a>
           </li>
           <li class="nav__menu-items">
-            <a class="nav__menu-items-link" href="">Bài viết </a>
+            <a class="nav__menu-items-link" href="post.php">Bài viết </a>
           </li>
           <li class="nav__menu-items nav__menu-2">
-            <a class="nav__menu-items-link" href="">Mẫu </a>
+            <a class="nav__menu-items-link" href="index.php">Mẫu </a>
             <svg width="12px" height="6px" viewBox="0 0 12 6" xmlns="http://www.w3.org/2000/svg">
               <polygon fill="#1F303C" points="5 5.7 0 0.7 0.7 0 5 4.4 9.3 0 10 0.7 "></polygon>
           </svg>
         
           </li>
           <li class="nav__menu-items">
-            <a class="nav__menu-items-link" href="">Trợ giúp</a>
+            <a class="nav__menu-items-link" href="contact.php">Trợ giúp</a>
           </li>
         </ul>
 
@@ -60,22 +60,22 @@
           </form>
           <ul class="sidebar__menu">
             <li class="sidebar__menu-items">
-              <a class="sidebar__menu-items-link" href=""><span>MỚI!</span></a>
+              <a class="sidebar__menu-items-link" href="index.php"><span>MỚI!</span></a>
             </li>
             <li class="sidebar__menu-items">
-              <a class="sidebar__menu-items-link" href=""><span>mẫu xanh lá</span></a>
+              <a class="sidebar__menu-items-link" href="index.php"><span>mẫu xanh lá</span></a>
             </li>
             <li class="sidebar__menu-items">
-              <a class="sidebar__menu-items-link" href=""><span>Mẫu màu xanh lam </span></a>
+              <a class="sidebar__menu-items-link" href="index.php"><span>Mẫu màu xanh lam </span></a>
             </li>
             <li class="sidebar__menu-items">
-              <a class="sidebar__menu-items-link" href=""><span>mẫu cam</span></a>
+              <a class="sidebar__menu-items-link" href="index.php"><span>mẫu cam</span></a>
             </li>
             <li class="sidebar__menu-items">
-              <a class="sidebar__menu-items-link" href=""><span>mẫu đỏ</span></a>
+              <a class="sidebar__menu-items-link" href="index.php"><span>mẫu đỏ</span></a>
             </li>
             <li class="sidebar__menu-items">
-              <a class="sidebar__menu-items-link" href=""><span>Mẫu hồng </span></a>
+              <a class="sidebar__menu-items-link" href="index.php"><span>Mẫu hồng </span></a>
             </li>
           </ul>
         </nav>
@@ -87,45 +87,45 @@
               mà bạn yêu thích</h1>
             <div class="row">
               <div class="col l-4 c-4 m-1">
-                <a href="">
+                <a href="templates/template1">
                   <div class="content">
-                    <img src="../src/img/Untitled.png" alt="">
+                    <img src="../src/img/Untitled.png" alt="template1">
                     
                   </div>
                 </a>
               </div>
               <div class="col l-4 c-4 m-1">
-                <a href="">
+                <a href="templates/template2">
                   <div class="content">
-                    <img src="../src/img/Untitled.png" alt="">
+                    <img src="../src/img/Untitled.png" alt="template2">
+                  </div>
+                </a>
+              </div>
+              <div class="col l-4 c-4 m-1">
+                <a href="templates/template3">
+                  <div class="content">
+                    <img src="../src/img/Untitled.png" alt="template3">
+                  </div>
+                </a>
+              </div>
+              <div class="col l-4 c-4 m-1">
+                <a href="templates/template4">
+                  <div class="content">
+                    <img src="../src/img/Untitled.png" alt="template4">
                   </div>
                 </a>
               </div>
               <div class="col l-4 c-4 m-1">
                 <a href="">
                   <div class="content">
-                    <img src="../src/img/Untitled.png" alt="">
+                    <img src="../src/img/Untitled.png" alt="template5">
                   </div>
                 </a>
               </div>
               <div class="col l-4 c-4 m-1">
-                <a href="">
+                <a href="templates/template1">
                   <div class="content">
-                    <img src="../src/img/Untitled.png" alt="">
-                  </div>
-                </a>
-              </div>
-              <div class="col l-4 c-4 m-1">
-                <a href="">
-                  <div class="content">
-                    <img src="../src/img/Untitled.png" alt="">
-                  </div>
-                </a>
-              </div>
-              <div class="col l-4 c-4 m-1">
-                <a href="">
-                  <div class="content">
-                    <img src="../src/img/Untitled.png" alt="">
+                    <img src="../src/img/Untitled.png" alt="template6">
                   </div>
                 </a>
               </div>
@@ -134,7 +134,10 @@
         </div>
       </div>
     </div>
+    <footer class="text-center section">
+            <div class="align-center grey-text my-4">&copy; bài tập lớn- kiều -Hoàng - ngọc-ĐẠI HỌC THỦY LỢI-175 TÂY SƠN ĐỐNG ĐA HÀ NỘI</div>
+    </footer>
   </div>
-  <script src="./Asset/active.js"></script>
+  <script src="public/active.js"></script>
 </body>
 </html>
