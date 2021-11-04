73d803374f552311c6f154b5ba2e097dc6192dbb
nguyenyenngoc <nyn11092001@gmail.com>
2021-11-04 15:27:53 +0700

    Báo cáo csdl

--- src/admin/controllers/catalog_blog/listController.php
+++ src/admin/controllers/catalog_blog/listController.php
@@ -7,16 +7,16 @@
 			$act = isset($_GET["act"])?$_GET["act"]:"";
 			switch ($act) {
 				case 'add':
-					$name = $_POST["topic"];
-					$metaTitle = RemoveString($name);
-					$this->Model->execute("insert into menu_catalog(name, metaTitle) values('$name','$metaTitle')");
+					$menucata_name = $_POST["topic"];
+					$menucata_metaTitle = RemoveString($menucata_name);
+					$this->Model->execute("insert into menu_catalog(name, metaTitle) values('$menucata_name','$menucata_metaTitle')");
 					// header("location: index.php?controller=catalog_blog/list");
 					echo "<meta http-equiv='refresh' content='0; URL=index.php?controller=catalog_blog/list'>";
 					break;
 
 				case 'delete':
-					$id = isset($_GET["id"])?$_GET["id"]:0;
-					$this->Model->execute("delete from menu_catalog where id=$id");
+					$menucata_id = isset($_GET["id"])?$_GET["id"]:0;
+					$this->Model->execute("delete from menu_catalog where id=$menucata_id");
 					echo "<meta http-equiv='refresh' content='0; URL=index.php?controller=catalog_blog/list'>";
 					break;
 			}
